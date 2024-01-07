8c846006965545d8755d6ae04b45c2edd053617a
Urenj <124129963+Urenj@users.noreply.github.com>
2024-01-07 11:40:10 +0800

    Add files via upload

---
+++ SHAPI/templates/footer.php
@@ -0,0 +1,40 @@
+     <!-- Footer -->
+     <footer class="page-footer grey darken-4 amber-text">
+        <div class="container">
+          <div class="row">
+            <div class="col l6 s12">
+              <h1 class="brand-logo amber-text center">AniNeru</h1>
+              <p class="amber-text center" style="font-size: 13pt;">S &nbsp;e &nbsp;l&nbsp;f &nbsp;&nbsp;T&nbsp;a&nbsp;u&nbsp;g&nbsp;h&nbsp;t&nbsp;&nbsp; A&nbsp;r&nbsp;t&nbsp;i&nbsp;s&nbsp;t&nbsp;&nbsp; &&nbsp;&nbsp; A&nbsp;n&nbsp;i&nbsp;m&nbsp;a&nbsp;t&nbsp;o&nbsp;r</p>
+            </div>
+            <div class="col l4 offset-l2 s12 center">
+              <h4 class="amber-text center">Links</h4>
+              <p class="contact" style="font-size: 13pt;">Contact me in these links below</p>
+              <ul>
+                <li>
+                  <a class="btn-floating btn-large waves-effect waves-darken pulse amber" href="./https://instagram.com/_anineru?igshid=NzZhOTFlYzFmZQ=="><i class="fa fa-instagram black-text" aria-hidden="true"></i></a>
+                  <a class="btn-floating btn-large waves-effect waves-darken pulse amber" href="./https://www.facebook.com/profile.php?id=100091417051963"><i class="fa fa-facebook-f black-text"></i></a>
+                  <a class="btn-floating btn-large waves-effect waves-darken pulse amber" href="./https://www.youtube. com/channel/UC4ODIPFkHWjTRx5Mkesu46w"><i class="fa fa-youtube-play black-text" aria-hidden="true"></i></a>
+                  <a class="btn-floating btn-large waves-effect waves-darken pulse amber" href="./https://l.facebook.com/l.php?u=https%3A%2F%2Ftwitter.com%2FAniNeru%3Ffbclid%3DIwAR0_Qu2bMnCqsay_BtrKFYo6Y8NKX7w1z1wrvHW-TiDotVpoV7QNpyq9CRE&h=AT3B6rHOM2stRkNDHmqmL9rr59w5bJWIhgaTYV5EltPeLJJGzTRe6YmYS9LOmGGqL4pP6YU1Pa0aZeC8a2CS4leJ0VZ7fWeEPI99BpxhF8OMHlnsC6_qLF0BcAplfeV_4RXiqd_c7Bv3nhD_OBkn">
+                    <i class="fa fa-twitter black-text" aria-hidden="true"></i></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="footer-copyright grey darken-4 amber-text">
+          <div class="container">
+            <div class=" center row col s12 m12 l12">© 2023 Copyright AniNeru</div>
+          <!-- <a class="grey-text text-lighten-4 right" href="#!">More Links</a> -->
+          </div>
+        </div>
+      </footer>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function(){
+        $('.sidenav').sidenav();
+      });
+    </script>
+</body>
---
+++ SHAPI/templates/header.php
@@ -0,0 +1,173 @@
+<head>
+	<title>AniNeru</title>
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" >
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+	<style type="text/css">
+		@import url('https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz&display=swap');
+
+        @import url('https://fonts.googleapis.com/css2?family=Borel&family=Dosis:wght@200;300;400;500;600;700;800&display=swap');
+
+        @import url('https://fonts.googleapis.com/css2?family=Titillium+Web&display=swap');
+
+        ::selection{
+          background-color: #c89703;
+          color: white;
+        }
+
+        .brand-logo{
+             font-family: Borel !important;
+        }
+
+        nav .brand-logo {
+            font-family: 'Borel';
+            padding: 12px 50px;
+        }
+
+        body {
+            font-family: Titillium Web;
+            margin: 0 !important;
+        }
+
+        .side-container {
+            display: flex;
+            height: 100vh;
+            top: 0;
+            bottom: 0;
+        }
+
+        .sidebar {
+            width: 230px;
+            overflow-y: auto;
+            position: fixed;
+            background-color: #333;
+            color: #fff;
+            padding: 20px;
+            box-sizing: border-box;
+            height: 100%;
+        }
+
+        .container {
+            flex: 1;
+            padding: 20px;
+            box-sizing: border-box;
+            margin-left: 250px; /* Adjusted to match the width of the sidebar */
+        }
+
+        .content{
+            margin-top: 0;
+            padding-left: 30px;
+            font-size: 13px;
+            margin-bottom: 20px; /* Added margin at the bottom to separate from the footer */
+        }
+
+        /* Add some styles for better visualization */
+        a {
+            color: #fff;
+            text-decoration: none;
+        }
+
+        .sidebar a:hover {
+            color: #ffc40c;
+        }
+
+        .sidebar ul {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+        }
+
+        .sidebar li {
+            margin-bottom: 10px;
+        }
+
+        .content {
+            margin-top: 0px !important;
+          margin-left: 210px; /* Same as the width of the sidenav */
+          font-size: 13px; /* Increased text to enable scrolling */
+          padding: 0px 30px;
+        }
+
+        .page-footer {
+            width: 100%;
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 20px;
+            box-sizing: border-box;
+            clear: both; /* Clear floating elements */
+            position: relative; /* Make sure it stays at the bottom */
+            z-index: 1;
+        }
+
+    </style>
+</head>
+<body>
+
+    <div class="side-container">
+        <div class="sidebar">
+            <div class="section">
+                    <h2>Sidebar</h2>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            
+        </div>
+
+    </div>
+
+
